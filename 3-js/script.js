d374685f52ccad41e07839df5c41d0ae20830c40
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-05-05 12:35:28 +0900

    Outside javascript commit

---
+++ 3-js/script.js
@@ -0,0 +1,12 @@
+      function average(a, b){
+        return((a+b)/2);
+      }
+      var x = 5;
+      var y = 10;
+      // alert((x+y)/2);
+      alert(average(x,y));
+
+      x = 8;
+      y = 12;
+      // alert((x+y)/2);
+      alert(average(x,y));
